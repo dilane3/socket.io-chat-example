a4677923e32ffd54cce4910c164e919c79128f2e
dilane3 <komboudilane125@gmail.com>
2021-11-22 20:15:14 +0100

    adding sound and group functionality

--- index.js
+++ index.js
@@ -7,18 +7,31 @@ const io = new Server(server)
 
 const PORT = process.env.PORT || 5000
 
-const users = []
+const colors = [
+  "red",
+  "blue",
+  "green",
+  "lightgreen",
+  "orange",
+  "violet",
+  "yellow",
+  "brown"
+]
 
 app.set("view engine", "ejs")
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
+app.use("/sound", express.static("public"))
+
 app.get("/", (req, res) => {
   const {name, room} = req.query
 
+  const idColor = Math.floor(Math.random() * colors.length)
+
   if (name && room)
-    res.render("index", {name, req})
+    res.render("index", {name, room, color: colors[idColor]})
   else
     res.redirect("/login")
 })
@@ -29,10 +42,13 @@ app.get("/login", (req, res) => {
 
 io.on("connection", (socket) => {
   console.log("new connexion detected")
-  console.log(socket.id)
+ 
+  socket.on("join", ({name, room}) => {
+    socket.join(room)
+  })
 
-  socket.on("chat message", (message) => {
-    socket.broadcast.emit("message", message)
+  socket.on("chat message", ({message, name, color, room}) => {
+    socket.broadcast.to(room).emit("message", {message, name, color})
   })
 
   socket.on("disconnect", (reason) => {
